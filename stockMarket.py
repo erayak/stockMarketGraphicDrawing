147fcae18fa37f6186d0d5dea65ee12be6e39a78
Eray Akyıldız <akyildiz379@gmail.com>
2018-05-02 23:46:04 +0300

    update stockMarket.py

--- stockMarket.py
+++ stockMarket.py
@@ -3,8 +3,8 @@
 
 # Kütüphaneler
 import matplotlib.pyplot as plt
-import urllib2 
-import json 
+import urllib2
+import json
 import numpy
 
 # Verileri Çekiyoruz
@@ -12,20 +12,20 @@ contents = urllib2.urlopen("https://www.doviz.com/api/v1/stocks/all/latest").rea
 data = json.loads(contents)
 
 # İslem Seçiyoruz
-islem = input("Yapmak İstediğiniz İşlemi Seçiniz")
+islem = input("Yapmak İstediğiniz İşlemi Seçiniz (1)Grafik çizer (2)Gün içi verilerini görüntüler: ")
 
 # Veri Alıyoruz
-hisse = raw_input("Hisse Adı Giriniz:")
+hisse = raw_input("Hisse Adı Giriniz: ")
 
 if islem == 1:
     for i in range(len(data)):
-        if hisse == data[i]["ticker"]:
+        if hisse.upper() == data[i]["ticker"]:
             plt.plot([data[i]["latest"],data[i]["lowest"],data[i]["highest"]])
             plt.show()
 
 elif islem == 2:
     for i in range(len(data)):
-        if hisse == data[i]["ticker"]:
+        if hisse.upper() == data[i]["ticker"]:
             print data[i]["full_name"]
             print "Günsonu: ",data[i]["latest"]
             print "Gün İçi En Yüksek: ",data[i]["highest"]
